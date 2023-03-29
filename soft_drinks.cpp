ecb49c9625754b33ce07bb856426e057d46916c0
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-03-29 19:53:49 +0530

    Solved

--- soft_drinks.cpp
+++ soft_drinks.cpp
@@ -1,16 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-    int k,l,c,d,p,nl,np;
-    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
-    int x=k*l;
-    int y=x/n;
-    int z=c*d;
-    int s=p/np;
-    cout<<min(y,min(z,s))/3<<endl;
+    int k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    cout << min(min(k * l / nl, c * d), p / np) / n << endl;
     return 0;
 }
 
