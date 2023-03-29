edf1997df476ff75e492b292b83a25776b5f32fa
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-03-30 03:37:11 +0530

    Solved

---
+++ Anton_and_Polyhrone.cpp
@@ -0,0 +1,42 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    string str;
+
+    while (cin >> n)
+    {
+        sum = 0;
+
+        while (n--)
+        {
+            cin >> str;
+
+            if (str == "Tetrahedron")
+                sum += 4;
+            else if (str == "Cube")
+                sum += 6;
+            else if (str == "Octahedron")
+                sum += 8;
+            else if (str == "Dodecahedron")
+                sum += 12;
+            else if (str == "Icosahedron")
+                sum += 20;
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
