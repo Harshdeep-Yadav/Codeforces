f3ef1cda2c6c3b4d746d9ff2697118a3e1632f58
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-04-04 19:52:38 +0530

    Solved

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,21 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t;
-    cin>>t;
-    int a,b,c,f,s;
-    while(t--){
-        f=-1,s=-1;
-        cin>>a>>b>>c;
-        if(a<c){
-            f=1;
-        }
-        if(a*b>c){
-            s=b;
-        }
-        cout<<f<<" "<<s<<endl;
-    }
+int main(){
+    long long n;
+    cin>>n;
+    cout<<(n-1)*n*(n+1)/6+n<<'\n';
     return 0;
 }
