ed394a00ec47b8abd1022e07ffd7ff61df34bdaa
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-04-07 23:23:25 +0530

    Solved

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,30 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-int main()
-{
-    
-    return 0;
-}
+/*-------------------------- Template-1 -----------------------------*/
+
+// #include <bits/stdc++.h>
+// define ll long long 
+// using namespace std;
+
+// int main(){
+// 
+//     return 0;
+// }
+
+
+/*-------------------------- Template-1 -----------------------------*/
+
+
+//  #include <bits/stdc++.h>
+// define ll long long 
+// #define pb push_back
+// using namespace std;
+
+// int main(){
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+        
+//     }
+//     return 0;
+// }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    long long n;
-    cin>>n;
-    cout<<(n-1)*n*(n+1)/6+n<<'\n';
-    return 0;
-}
+			
