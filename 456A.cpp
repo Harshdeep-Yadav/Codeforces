fb16a63ef7782f18734f8e8ac2adae387301d2d2
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-05-01 18:48:41 +0530

    Solved

---
+++ 456A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+   vector< pair<int,int>> pr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>pr[i].first>>pr[i].second;
+    }
+    sort(pr.begin(),pr.end());
+
+
+    for(int j=0;j<n-1;j++)
+    {
+        if(pr[j].first<pr[j+1].first && pr[j].second>pr[j+1].second)
+        {
+            cout<<"Happy Alex"<<endl;
+            return 0;
+        }
+    }
+    cout<<"Poor Alex"<<endl;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,12 @@
 
-                    even++;
-                }
-            }
-            else
-            {
-                if (i % 2 == 1)
-                {
-                    good++;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    ll a[n];
+    ll b[n];
+    for(int i = 0; i < n; i++){
+        cin>>a[i];
+    }
+    for(int i = 0; i < n; i++){
