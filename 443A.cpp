8a455727332b4d2ad456d83e53926e0159a8dbf7
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-05-06 00:14:12 +0530

    Solved

--- 443A.cpp
+++ 443A.cpp
@@ -1,17 +1,22 @@
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin>>s;
-//     set<char>st;
-//     for(int i=0;i<s.size();i++){
-//         if(s[i]>='a' && s[i]<='z')
-//         st.insert(s[i]);
-//     }
-//     cout<<st.size()<<endl;
+int main(){
+    string s;
+    cin>>s;
+    set<char>st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='a' && s[i]<='z')
+        st.insert(s[i]);
+    }
+    cout<<st.size()<<endl;
    
-// }
+}
+
+
+/******************************************************/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
