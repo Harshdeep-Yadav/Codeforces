e5274160c67edcf8dfd5290dd8863b5b7064baea
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-06-10 12:44:22 +0530

    Solved

--- 478B.cpp
+++ 478B.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long 
+#define ll long long
 int main()
 {
     ll n, m;
     cin >> n >> m;
 
-    if(m==1){
-        ll res=(n*(n-1))/2;
-        cout<<res<<res<<endl;
+    if (m == 1)
+    {
+        ll res = (n * (n - 1)) / 2;
+        cout << res << " " << res << endl;
+        return 0;
     }
-    
+    ll maxi = ((n - m + 1) * (n - m)) / 2;
+    ll x = n / m;
+    ll y = n % m;
+
+    ll res1 = (x * (x - 1)) / 2;
+    ll res2 = ((x + 1) * (x)) / 2;
+    ll res3 = (res1 * (m - y)) + (res2 * (y));
+
+    cout << res3 << " " << maxi << endl;
+    return 0;
 }
