94209632a29643bf06615187a13720d85a2a8202
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-07-08 01:42:36 +0530

    Solved

---
+++ 579A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >>x;
+    int cnt=0;
+    while(x/2!=0){
+        if(x%2==1){
+            cnt++;
+        }
+        x/=2;
+    }
+    cout<<cnt+1;
+}
