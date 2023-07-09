94b351638f0e312b29506c25417ae4f09e7faff9
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-07-10 02:51:49 +0530

    Solve

--- 519B.cpp
+++ 519B.cpp
@@ -1,7 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int n, x;
+    cin >> n;
+    int a = 0, b = 0, c = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        a += x;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> x;
+        b += x;
+    }
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        cin >> x;
+        c += x;
+    }
+
+    cout << a - b << endl;
+    cout << b - c << endl;
+
     return 0;
-} 
+}
